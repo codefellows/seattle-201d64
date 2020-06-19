5e33ad64da906c214da3a8726ae7052805fa362f
Nicholas Carignan <nick.carignan@sbcglobal.net>
2020-06-19 09:44:32 -0700

    day 15 cr

---
+++ class-15/code-review/js/app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+/*
+  approaches :
+    looked for easy things to do so we would have things to work on
+    reading through all of it showed us how it all tied together
+    - prevented rewriting something that existed
+    - see if something looked like an earlier project, compare them side by side
+    - look over project and see what relied on each other. When really stuck, moved to another place
+
+    - Find what is being called (and see if its complete)
+*/
+
+
+// Cart constructor.
+var Cart = function(items) {
+  // this.items is an array of CartItem instances.
+  this.items = items;
+};
+
+Cart.prototype.addItem = function(product, quantity) {
+  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  var newItem = new CartItem(product, quantity);
+  this.items.push(newItem);
+  console.log(this.items);
+};
+
+Cart.prototype.saveToLocalStorage = function() {
+  // TODO: Fill in this instance method to save the contents of the cart to localStorage
+
+  var itemsICanUse = this.items;
+
+  // 1. stringify the data
+  var stringyItems = JSON.stringify(itemsICanUse);
+  // 2. set the data with setItem
+  localStorage.setItem('cart', stringyItems);
+};
+
+Cart.prototype.removeItem = function(item) {
+  // TODO: Fill in this instance method to remove one item from the cart.
+  // Note: You will have to decide what kind of parameter to pass in here!
+};
+
+var CartItem = function(product, quantity) {
+  this.product = product;
+  this.quantity = quantity;
+};
+
+// Product contructor.
+var Product = function(filePath, name) {
+  this.filePath = filePath;
+  this.name = name;
+  Product.allProducts.push(this);
+};
+Product.allProducts = [];
+
+function generateCatalog() {
+  new Product('assets/bag.jpg', 'Bag');
+  new Product('assets/banana.jpg', 'Banana');
+  new Product('assets/bathroom.jpg', 'Bathroom');
+  new Product('assets/boots.jpg', 'Boots');
+  new Product('assets/breakfast.jpg', 'Breakfast');
+  new Product('assets/bubblegum.jpg', 'Bubblegum');
+  new Product('assets/chair.jpg', 'Chair');
+  new Product('assets/cthulhu.jpg', 'Cthulhu');
+  new Product('assets/dog-duck.jpg', 'Dog-Duck');
+  new Product('assets/dragon.jpg', 'Dragon');
+  new Product('assets/pen.jpg', 'Pen');
+  new Product('assets/pet-sweep.jpg', 'Pet Sweep');
+  new Product('assets/scissors.jpg', 'Scissors');
+  new Product('assets/shark.jpg', 'Shark');
+  new Product('assets/sweep.png', 'Sweep');
+  new Product('assets/tauntaun.jpg', 'Taun-Taun');
+  new Product('assets/unicorn.jpg', 'Unicorn');
+  new Product('assets/usb.gif', 'USB');
+  new Product('assets/water-can.jpg', 'Water Can');
+  new Product('assets/wine-glass.jpg', 'Wine Glass');
+}
+
+// Initialize the app by creating the big list of products with images and names
+generateCatalog();
